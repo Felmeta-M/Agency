3ce208dcecb58a67e6223663ff4bad13f00e24ec
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-09-28 15:03:59 +0300

    main page

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,6 +6,9 @@ const Container = styled.div`
     height: calc(100vh - 50px);
     display: flex;
     padding: 20px;
+    @media only screen and (max-width: 480px) {
+        flex-direction: column;
+    }
 `;
 const Left = styled.div`
     width: 60%;
@@ -13,15 +16,26 @@ const Left = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    @media only screen and (max-width: 480px) {
+        width: 100%;
+        height: 100%;
+    }
 `;
 const Title = styled.h1`
     width: 60%;
     font-size: 60px;
+    @media only screen and (max-width: 480px) {
+        width: 100%;
+        font-size: 50px;
+    }
 `;
 const Desc = styled.p`
     width: 60%;
     font-size: 20px;
     margin-top: 60px;
+    @media only screen and (max-width: 480px) {
+        width: 100%;
+    }
 `;
 const Info = styled.div`
     width: 60%;
@@ -29,6 +43,10 @@ const Info = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    @media only screen and (max-width: 480px) {
+        width: 100%;
+        flex-direction: column;
+    }
 `;
 const Button = styled.button`
     padding: 15px;
@@ -39,14 +57,17 @@ const Button = styled.button`
     border: none;
     letter-spacing: 2px;
     cursor: pointer;
+    @media only screen and (max-width: 480px) {
+        margin-bottom: 20px;
+        border: 2px solid white;
+    }
 `;
 const Contact = styled.div`
     display: flex;
     flex-direction: column;
 `;
 const Phone = styled.span`
-    color: #00ff99;
-    font-weight: bold;
+    color: black;
 `;
 const ContactText = styled.span`
     color: gray;
@@ -55,6 +76,9 @@ const ContactText = styled.span`
 
 const Right = styled.div`
     width: 40%;
+    @media only screen and (max-width: 480px) {
+        display: none;
+    }
 `;
 const Image = styled.img`
     margin-top: 85px;
@@ -65,14 +89,15 @@ const Intro = () => {
   return (
     <Container>
         <Left>
-            <Title>Adventures in creative age</Title>
-            <Desc>We believe that designing products and services in close partenership
-                with our clients is the only way to have a real impact on their business.
+            <Title>Explorations in the era of creativity</Title>
+            <Desc>
+            We believe that collaborating closely with our clients in the design of 
+            products and services is the key to making a meaningful impact on their business.
             </Desc>
             <Info>
                 <Button>Start A Project</Button>
                 <Contact>
-                    <Phone>Call Us (+251924193212)</Phone>
+                    <Phone>Call Us (+251922434343)</Phone>
                     <ContactText>For any question or concern</ContactText>
                 </Contact>
             </Info>
