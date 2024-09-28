ad98bf3090352a48f879f0e63ab0d2862f11d9ac
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-09-28 15:03:31 +0300

    features

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -4,9 +4,16 @@ import AnimatedShapes from "./AnimatedShapes";
 
 const Container = styled.div`
     display: flex;
+    @media only screen and (max-width: 480px) {
+        flex-direction: column;
+        padding: 30px 20px;
+    }
 `;
 const Left = styled.div`
     width: 50%;
+    @media only screen and (max-width: 480px) {
+        display: none;
+    }
 `;
 const Image = styled.img`
     width: 76%;
@@ -16,19 +23,28 @@ const Right = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    @media only screen and (max-width: 480px) {
+        width: 100%;
+        // align-items: center;
+        justify-content: center;
+    }
 `;
 const Title = styled.span`
     font-size: 70px;
+    @media only screen and (max-width: 480px) {
+        font-size: 50px;
+    }
 `;
 const SubTitle = styled.span`
     font-size: 24px;
     font-style: italic;
-    color: gray;
+    color: #999;
     margin-top: 30px;
+    font-weight: bold;
 `;
 const Desc = styled.p`
     font-size: 20px;
-    color: #888;
+    color: #999;
     margin-top: 30px;
 `;
 const Button = styled.button`
@@ -41,6 +57,11 @@ const Button = styled.button`
     border-radius: 10px;
     margin-top: 20px;
     cursor: pointer;
+    @media only screen and (max-width: 480px) {
+        border: 2px solid white;
+        margin-top: 70px;
+        margin-left: 100px;
+    }
 `;
 
 const Feature = () => {
@@ -55,11 +76,14 @@ const Feature = () => {
             </Title>
             <SubTitle> We know that good design means good business.</SubTitle>
             <Desc>
-                We help our clients succeed by creating brand identities, digital experiances
-            , and print materials that communicate clearly, achieve marketing goals, and look fantastic.
+            We help our clients succeed by developing brand identities, digital experiences, 
+            and print materials that convey messages clearly, meet marketing objectives, and look amazing.
             </Desc>
             <Desc>
-                We care your business and guarantee you to achieve marketing goals.
+            We care about your business and guarantee success in achieving your marketing goals.
+            </Desc>
+            <Desc>
+            Our goal is to help your business grow and reach a wider audience.
             </Desc>
             <Button>Learn More</Button>
         </Right>
